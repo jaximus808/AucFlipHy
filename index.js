983b347636d48b84b5d26d5f6637f1cf21870b7c
jaximus808 <68038808+jaximus808@users.noreply.github.com>
2021-06-15 10:16:13 -1000

    Update index.js

--- index.js
+++ index.js
@@ -7,42 +7,6 @@ client.once('ready', () =>{
     console.log('Start af');
     
 })
-
-
-// client.on('message', message =>{
-
-//     var sortedAuctionData = {};
-//     var sortedBinData = {};
-
-//     var flipObs = {};
-//     function handleDataOne(totalPages)
-//     {
-//         for(let i = 0; i < totalPages; i++)
-//         {
-//             fetch(`https://api.hypixel.net/skyblock/auctions?page=${i}`)
-//             .then(res =>res.json()).then((json) => {SortData(json, i,json.auctions.length ); })
-//         }
-
-//     }
-    
-//     if(message.author.bot) return;
-//     let input = message.content;
-//     var stringArray = input.toLowerCase().split(' ');
-//     if(stringArray[0] == "-af")
-//     {
-//         if(stringArray.length == 1)
-//         {
-//             message.channel.send("Input more parameters")
-//             return;
-//         }
-        
-//         //first fetch to get total pages
-//         fetch("https://api.hypixel.net/skyblock/auctions?page=0")
-//         .then(res =>res.json()).then((json) => {handleDataOne(json.totalPages); })
-        
-//     }
-// })
-
 client.on('guildMemberUpdate', (oldMember, newMember) => {
     
     // if(newMember.nickname && oldMember.nickname !== newMember.nickname) {
@@ -50,10 +14,7 @@ client.on('guildMemberUpdate', (oldMember, newMember) => {
     //         newMember.setNickname('NickName')
     //     }
     // }
-    if(newMember.user.id == 413398657791164416)
-    {
-        newMember.setNickname("Protagonist");
-    }
+    
     if(newMember.user.id != 279828346240630786) return; 
     newMember.setNickname("NegativeTalent");
  });
